bc70293e34767eaaa63e9246786e07c73817a0bb
shubham <shubhambhade5023@gmail.com>
2025-11-03 10:55:15 +0530

    dvc intiated with data version1

--- code.py
+++ code.py
@@ -18,6 +18,7 @@ df = pd.DataFrame(data)
 # df.loc[len(df.index)] = new_row_loc2
 
 # Ensure the "data" directory exists at the root level
+
 data_dir = 'data'
 os.makedirs(data_dir, exist_ok=True)
 
